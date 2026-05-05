1681ab5b208c16bf02ca269fe19cfb5449065192
anderson <andersonbc97@gmail.com>
2026-05-04 22:19:54 -0300

    criação das tabelas no banco de dados com fliway

--- src/main/resources/db/migration/V1__create_academia_schema.sql
+++ src/main/resources/db/migration/V1__create_academia_schema.sql
@@ -17,4 +17,67 @@ CREATE TABLE alunos(
     atualizado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE modalidades(
+    id BIGSERIAL PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL,
+    ativa BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE graduacoes(
+    id BIGSERIAL PRIMARY KEY,
+    modalidade_id BIGINT NOT NULL REFERENCES modalidades(id),
+    nome VARCHAR(150) NOT NULL,
+    UNIQUE (modalidade_id, nome)
+);
+
+
+CREATE TABLE planos(
+    id BIGSERIAL PRIMARY KEY,
+    modalidade_id BIGINT NOT NULL REFERENCES modalidades(id),
+    nome VARCHAR(150) NOT NULL,
+    valor_mensal NUMERIC(10,2) NOT NULL CHECK(valor_mensal >= 0),
+    ativo BOOLEAN NOT NULL DEFAULT TRUE,
+    UNIQUE (modalidade_id,nome)
+);
+
+CREATE TABLE matriculas(
+    id BIGSERIAL PRIMARY KEY,
+    aluno_id BIGINT NOT NULL REFERENCES alunos(id),
+    data_matricula DATE NOT NULL DEFAULT CURRENT_DATE,
+    dia_vencimento INTEGER NOT NULL CHECK(dia_vencimento BETWEEN 1 AND 31),
+    data_encerramento DATE,
+    status VARCHAR(20) NOT NULL DEFAULT 'ATIVA',
+    CHECK (status IN ('ATIVA','ENCERRADA','CANCELADA'))
+);
+
+CREATE TABLE matriculas_modalidade(
+    id BIGSERIAL PRIMARY KEY,
+    matricula_id BIGINT NOT NULL REFERENCES matriculas(id),
+    modalidade_id BIGINT NOT NULL REFERENCES modalidades(id),
+    graduacao_id BIGINT NOT NULL REFERENCES graduacoes(id),
+    plano_id BIGINT NOT NULL REFERENCES planos(id),
+    data_inicio DATE NOT NULL DEFAULT CURRENT_DATE,
+    data_fim DATE,
+    UNIQUE (matricula_id, modalidade_id)
+);
+
+CREATE TABLE faturas_matriculas(
+    id BIGSERIAL PRIMARY KEY,
+    matricula_id BIGINT NOT NULL REFERENCES matriculas(id),
+    data_vencimento DATE NOT NULL,
+    valor NUMERIC(10,2) NOT NULL CHECK(valor >= 0),
+    data_pagamento TIMESTAMP,
+    data_cancelamento DATE,
+    status VARCHAR(20) NOT NULL DEFAULT 'ABERTA',
+    CHECK (status IN ('ABERTA','PAGA','CANCELADA', 'VENCIDA')),
+    UNIQUE (matricula_id, data_vencimento)
+);
+
+CREATE TABLE assiduidade(
+    id BIGSERIAL PRIMARY KEY,
+    matricula_id BIGINT NOT NULL REFERENCES matriculas(id),
+    data_entrada TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    data_saida TIMESTAMP
+);
+
 
